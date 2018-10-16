7cefeb653fc760f5ecc02ddfea9c029414e7f041
aman_agrawal <aman.agarwal@timesinternet.in>
2018-10-17 00:25:05 +0530

    Removing ignored files

---
+++ client/build/precache-manifest.a3b4b4db5de03d48739d8b4268ffd603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86b4a095ac973523e58a",
+    "url": "/static/js/main.86b4a095.chunk.js"
+  },
+  {
+    "revision": "8a8201d36d38b4108721",
+    "url": "/static/js/1.8a8201d3.chunk.js"
+  },
+  {
+    "revision": "86b4a095ac973523e58a",
+    "url": "/static/css/main.46a4d9b0.chunk.css"
+  },
+  {
+    "revision": "ca04e14c682b9f22612417039f9388ca",
+    "url": "/index.html"
+  }
+];
