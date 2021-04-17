1d8159dd0c3bc4290d9261860cb216985a681131
aman-agrwl <amanagrawal.am@gmail.com>
2021-04-17 11:51:43 +0530

    Udpated code

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('client/build'));
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}.`);
